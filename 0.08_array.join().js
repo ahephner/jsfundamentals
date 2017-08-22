33de4101f75d6d6ab01de18a0844a7b0fc2f4bfc
AJ Hephner <ajhephner178@gmail.com>
2017-08-22 08:49:27 -0400

    added another js method

---
+++ 0.08_array.join().js
@@ -0,0 +1,11 @@
+//this method .join(); returns the value of the array as a string
+var myArray=[23,"AJ", "Jackie"];
+
+console.log(myArray.join());
+
+//using the ("+"); adds strings together
+console.log(myArray.join("+"));
+
+// this (""); this combines elements into one string
+
+console.log(myArray.join(""));
