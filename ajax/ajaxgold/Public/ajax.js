f7036e3f503866f8790e2cc3add9359b7a87fc1a
AJ Hephner <ajhephner178@gmail.com>
2017-08-29 11:58:58 -0400

    finally got table up on Ajax gold. Good code on how to grab api and add to table

--- ajax/ajaxgold/Public/ajax.js
+++ ajax/ajaxgold/Public/ajax.js
@@ -1,51 +1,50 @@
 $(document).ready(function(){
-
-
-
+	beerList = []
 
 	$.ajax({
 		type: 'GET',
 		url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
 	}).done(function(data){
-		console.log(data)
 		data.forEach(function (a){
 			$('#author').append('<h1>' + a.title + '</h1>');
 			$('#quote').append('<p>' + a.content +'</p>');
 			// $('#link').append('<h3>' + a.link + '</h3>');
 		})
+	})
 
+	
+	$.ajax({
+		type: 'GET',
+		url: 'https://lcboapi.com/products?access_key=MDo3MDc5OWRmYy04YzFmLTExZTctYTUxNC0zYmJmZDRkOWM0MzI6RHZHbGI0QjhSMWVsWGFjYW9KZ0kyakx3a244WFgyRWl3cmpO'
+		}).done(function(data){
+			data.result.forEach(function(beer){
+				let newBeer = {
+					name: beer.name,  //creating a value in new object to store what the element we want. In this case the API had an array that had name. so beer.name grabbed the name beer came from the function. It was the parametere we pushed through. 
+					origin: beer.origin,  //creating a value for origin again we made up the name origin: beer.origin is from pass the parameter beer through .forEachfunction
+					varietal: beer.varietal  //creating a value for varietal. Again we can console.log at any point and see if we are grabbing by console.log(beer.varietal)
+				}
+				beerList.push(newBeer)		//pushing elements from function to created array outside the function. 
+			})
+	}).done(function(){
+		beerList.forEach(function(beer){
+
+			let tr = document.createElement('tr')  //creating a new table row
+			let name_td = document.createElement('td')  //creating a table data under notice name origin_td
+			let origin_td = document.createElement('td')
+			let varietal_td = document.createElement('td')
+
+			name_td.innerHTML = beer.name  
+			origin_td.innerHTML = beer.origin
+			varietal_td.innerHTML = beer.varietal
+
+			tr.appendChild(name_td)  //adding name_td to tr
+			tr.appendChild(origin_td)
+			tr.appendChild(varietal_td)
+
+			console.log(tr)
+			// document.querySelector('#beerTable').appendChild(tr) //vanilla javascript
+
+			$('#beerTable').append(tr) // Jquery method
 		})
 	})
-	
-	
-
-
-
-
-// 		$(searchName).on('click', function(){
-// 			let search = $(searchRes).val();
-
-
-
-// TRYING TO ADD SEARCH FUNCTION BELOW
-// 	$.ajax({
-// 		type: 'GET',
-// 		url: 'https://lcboapi.com/products?access_key=MDo3MDc5OWRmYy04YzFmLTExZTctYTUxNC0zYmJmZDRkOWM0MzI6RHZHbGI0QjhSMWVsWGFjYW9KZ0kyakx3a244WFgyRWl3cmpO'
-// 	}).done(function(data){
-// 		$("#searchResult").html("")
-// 		 for (let d in data.results [0]){
-// 		 	let dataName = '<h3 class="searchRe">' + d + ':</h3>'
-// 		 	$(searchResult).append(dataName + "<p>" + data.results[0][d] + "</p>")
-// 		 }
-		
-
-// 		})
-
-// })
-
-
-
-	
-	
-
-	
+})
